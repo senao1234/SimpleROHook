9079ad3dd7ed576b687d589c2b848a8eeb5a3c5f
X-EcutiOnner <xex.ecutionner@gmail.com>
2020-12-06 21:51:34 +0700

    Update coding style in Injection\Core\ro\map.h
    Signed-off-by: X-EcutiOnner <xex.ecutionner@gmail.com>

--- Injection/Core/ro/map.h
+++ Injection/Core/ro/map.h
@@ -1,14 +1,15 @@
 #pragma once
 
-struct CLightmap {
+struct CLightmap
+{
 	unsigned char brightObj[3];
 	CTexture* surface;
 	struct texCoor coor[4];
 	struct COLOR intensity[4];
 };
 
-
-struct CAttrCell {
+struct CAttrCell
+{
 	float h1;
 	float h2;
 	float h3;
@@ -16,8 +17,8 @@ struct CAttrCell {
 	int flag;
 };
 
-
-class C3dAttr : public CRes {
+class C3dAttr : public CRes
+{
 public:
 	int m_width;
 	int m_height;
@@ -28,50 +29,57 @@ public:
 	C3dAttr(class C3dAttr&) {};
 	C3dAttr::C3dAttr(class Exemplar __formal, char* resid, char* baseDir) {};
 	C3dAttr::C3dAttr() {};
+
 	void Create(int w, int h)
 	{
-		m_cells.reserve( w * h );
+		m_cells.reserve(w * h);
 	}
-	float C3dAttr::GetHeight( float x, float z)	{};
 
-	int C3dAttr::GetAttr( float x, float z){
-		long cx,cy;
-		ConvertToCellCoor(x,z,cx,cy);
-		return m_cells[ cx + cy * m_width ].flag;
+	float C3dAttr::GetHeight(float x, float z) {};
+
+	int C3dAttr::GetAttr(float x, float z)
+	{
+		long cx, cy;
+		ConvertToCellCoor(x, z, cx, cy);
+
+		return m_cells[cx + cy * m_width].flag;
 	};
 
 	struct CAttrCell* GetCell(int Cellx, int Celly)
 	{
-		return &m_cells[ Cellx + Celly * m_width ];
-	};
-	float C3dAttr::RayTest( struct ray3d& ray, int x, int y) {};
-
-	void C3dAttr::SetCellInfo( int Type, int Cellx, int Celly)
-	{
-		m_cells[ Cellx + Celly * m_width ].flag = Type;
+		return &m_cells[Cellx + Celly * m_width];
 	};
 
-	void C3dAttr::GetWorldPos( float attrX, float attrY, struct vector2d& pos)
+	float C3dAttr::RayTest(struct ray3d& ray, int x, int y) {};
+
+	void C3dAttr::SetCellInfo(int Type, int Cellx, int Celly)
 	{
-		pos.x = ((attrX - m_width / 2) * (float)m_zoom) + 2.5f;
+		m_cells[Cellx + Celly * m_width].flag = Type;
+	};
+
+	void C3dAttr::GetWorldPos(float attrX, float attrY, struct vector2d& pos)
+	{
+		pos.x = ((attrX - m_width  / 2) * (float)m_zoom) + 2.5f;
 		pos.y = ((attrY - m_height / 2) * (float)m_zoom) + 2.5f;
 	};
+
 	void ConvertToCellCoor(float x, float z, long& cx, long& cy)
 	{
-		cx = (long)( x / m_zoom + m_width /2 );
-		cy = (long)( z / m_zoom + m_height /2 );
+		cx = (long)(x / m_zoom + m_width  / 2);
+		cy = (long)(z / m_zoom + m_height / 2);
 	};
-	void C3dAttr::GetHeightMinMax( struct tagRECT& area, float& topHeight, float& bottomHeight) {};
-	class C3dAttr& operator=(class C3dAttr&) {};
+
+	void C3dAttr::GetHeightMinMax(struct tagRECT& area, float& topHeight, float& bottomHeight) {};
+	class C3dAttr& operator = (class C3dAttr&) {};
 
 	virtual class CRes* C3dAttr::Clone() {};
-	virtual unsigned char C3dAttr::Load( char* fName) {};
+	virtual unsigned char C3dAttr::Load(char* fName) {};
 	virtual void C3dAttr::Reset() {};
 	virtual C3dAttr::~C3dAttr() {};
 };
 
-
-class CLightmapMgr {
+class CLightmapMgr
+{
 public:
 	std::vector<CLightmap> m_lightmaps;
 	std::vector<CTexture*> m_lmSurfaces;
@@ -81,12 +89,13 @@ public:
 	CLightmapMgr(class CLightmapMgr&) {};
 	CLightmapMgr::CLightmapMgr() {};
 	CLightmapMgr::~CLightmapMgr() {};
-	void CLightmapMgr::Create( class CGndRes* gnd) {};
+	void CLightmapMgr::Create(class CGndRes* gnd) {};
 	void CLightmapMgr::Reset() {};
-	class CLightmapMgr& operator=(class CLightmapMgr&) {};
+	class CLightmapMgr& operator = (class CLightmapMgr&) {};
 };
 
-class C3dGround {
+class C3dGround
+{
 public:
 	class C3dAttr* m_attr;
 	int m_width;
@@ -109,16 +118,19 @@ public:
 	virtual ~C3dGround() {};
 };
 
-
-class C3dNode {
-	struct ColorInfo {
+class C3dNode
+{
+	struct ColorInfo
+	{
 		unsigned long color[3];
 		struct COLOR argb[3];
 		struct vector2d uv[3];
 		ColorInfo() {};
 	};
+
 	class C3dActor* m_master;
-	void* m_mesh; //class C3dMesh* m_mesh;
+	void* m_mesh;
+	// class C3dMesh* m_mesh;
 	char m_name[128];
 	int m_numTexture;
 	std::vector<CTexture*> m_texture;
@@ -137,8 +149,8 @@ class C3dNode {
 	virtual ~C3dNode() {};
 };
 
-
-class C3dActor {
+class C3dActor
+{
 	class C3dNode* m_node;
 	char m_name[128];
 	struct vector3d m_pos;
@@ -166,7 +178,8 @@ class C3dActor {
 	virtual ~C3dActor() {};
 };
 
-struct SceneGraphNode {
+struct SceneGraphNode
+{
 	struct SceneGraphNode* m_parent;
 	struct SceneGraphNode* m_child[4];
 	struct C3dAABB m_aabb;
@@ -182,21 +195,22 @@ struct SceneGraphNode {
 	SceneGraphNode(struct SceneGraphNode&) {};
 	SceneGraphNode::SceneGraphNode() {};
 	SceneGraphNode::~SceneGraphNode() {};
-	void SceneGraphNode::Build( int level) {};
-	void SceneGraphNode::InsertObject( class C3dAttr* attr, int level) {};
-	void SceneGraphNode::InsertObject( class C3dGround* ground, int level) {};
-	void SceneGraphNode::InsertObject( class C3dActor* actor, int level) {};
-	void SceneGraphNode::RemoveObject( class C3dActor* actor, int level) {};
-	void SceneGraphNode::UpdateVolume( int level) {};
-	void SceneGraphNode::UpdateVolumeAfter( int level) {};
-	std::vector<C3dActor*>* SceneGraphNode::GetActorList( float x, float z, int level) {};
-	void SceneGraphNode::InsertObjectAfter( class C3dActor* actor, int level) {};
-	void SceneGraphNode::CopySceneGraph( int Level, struct SceneGraphNode* graph) {};
-	void SceneGraphNode::LoadSceneGraph( class CFile* fp, int Level) {};
-	struct SceneGraphNode& operator=(struct SceneGraphNode&) {};
+	void SceneGraphNode::Build(int level) {};
+	void SceneGraphNode::InsertObject(class C3dAttr* attr, int level) {};
+	void SceneGraphNode::InsertObject(class C3dGround* ground, int level) {};
+	void SceneGraphNode::InsertObject(class C3dActor* actor, int level) {};
+	void SceneGraphNode::RemoveObject(class C3dActor* actor, int level) {};
+	void SceneGraphNode::UpdateVolume(int level) {};
+	void SceneGraphNode::UpdateVolumeAfter(int level) {};
+	std::vector<C3dActor*>* SceneGraphNode::GetActorList(float x, float z, int level) {};
+	void SceneGraphNode::InsertObjectAfter(class C3dActor* actor, int level) {};
+	void SceneGraphNode::CopySceneGraph(int Level, struct SceneGraphNode* graph) {};
+	void SceneGraphNode::LoadSceneGraph(class CFile* fp, int Level) {};
+	struct SceneGraphNode& operator = (struct SceneGraphNode&) {};
 };
 
-class CViewFrustum {
+class CViewFrustum
+{
 public:
 	std::list<SceneGraphNode*> m_cubeletListTotal;
 	std::list<SceneGraphNode*> m_cubeletListPartial;
@@ -206,15 +220,17 @@ public:
 	CViewFrustum(class CViewFrustum&);
 	CViewFrustum::CViewFrustum();
 	void CViewFrustum::Free();
-	void CViewFrustum::Build( float hratio, float vratio, struct matrix& viewMatrix, struct SceneGraphNode* rootNode);
-	void CViewFrustum::CullSceneNode( struct SceneGraphNode* node, int level, unsigned char isTotallyInside);
-	int CViewFrustum::CheckAABBIntersect( struct SceneGraphNode* node);
-	int CViewFrustum::CheckOBBIntersect( struct C3dOBB& bb);
-	class CViewFrustum& operator=(class CViewFrustum&);
+	void CViewFrustum::Build(float hratio, float vratio, struct matrix& viewMatrix, struct SceneGraphNode* rootNode);
+	void CViewFrustum::CullSceneNode(struct SceneGraphNode* node, int level, unsigned char isTotallyInside);
+	int CViewFrustum::CheckAABBIntersect(struct SceneGraphNode* node);
+	int CViewFrustum::CheckOBBIntersect(struct C3dOBB& bb);
+	class CViewFrustum& operator = (class CViewFrustum&);
+
 	CViewFrustum::~CViewFrustum();
 };
 
-class CView {
+class CView
+{
 public:
 	float m_sideQuake;
 	float m_frontQuake;
@@ -233,7 +249,8 @@ public:
 	struct matrix m_invViewMatrix;
 	class CViewFrustum m_viewFrustum;
 	class CWorld* m_world;
-	void* m_skyBox;//class CSkyBoxEllipse* m_skyBox;
+	void* m_skyBox;
+	// class CSkyBoxEllipse* m_skyBox;
 
 	CView(class CView&) {};
 	CView::CView() {};
@@ -262,27 +279,29 @@ public:
 	void ResetAt(float, float, float) {};
 	void PushCamera() {};
 	void PopCamera() {};
-	void CView::AddLongitude( float deltaLongitude) {};
-	void CView::AddLatitude( float deltaLatitude) {};
-	void CView::AddDistance( float deltaDistance) {};
-	void CView::SetQuake( int isQuake, int Type, float sideQuake, float frontQuake, float latitudeQuake) {};
-	void CView::SetQuakeInfo( float sideQuake, float frontQuake, float latitudeQuake) {};
-	void CView::GetEeyeVector( struct vector3d* eyeVector) {};
+	void CView::AddLongitude(float deltaLongitude) {};
+	void CView::AddLatitude(float deltaLatitude) {};
+	void CView::AddDistance(float deltaDistance) {};
+	void CView::SetQuake(int isQuake, int Type, float sideQuake, float frontQuake, float latitudeQuake) {};
+	void CView::SetQuakeInfo(float sideQuake, float frontQuake, float latitudeQuake) {};
+	void CView::GetEeyeVector(struct vector3d* eyeVector) {};
 	struct vector3d* GetEeyeFromVector() {};
 	unsigned char IsFPSmode() {};
 	void CView::InterpolateViewInfo() {};
 	void CView::ProcessQuake() {};
 	void CView::BuildViewMatrix() {};
-	class CView& operator=(class CView&) {};
+	class CView& operator = (class CView&) {};
 
 	virtual CView::~CView() {};
+
 	virtual void CView::OnEnterFrame() {};
 	virtual void CView::OnExitFrame() {};
 	virtual void CView::OnRender() {};
 	virtual void CView::OnCalcViewInfo() {};
 };
 
-class CWorld {
+class CWorld
+{
 public:
 	class CMode* m_curMode;
 	std::list<CGameObject*> m_gameObjectList;
@@ -304,4 +323,3 @@ public:
 
 	virtual ~CWorld() {};
 };
-
