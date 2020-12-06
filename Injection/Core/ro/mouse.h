bf21bbf878928aee5cd92781491516accfbaa618
X-EcutiOnner <xex.ecutionner@gmail.com>
2020-12-06 21:55:12 +0700

    Update coding style in Injection\Core\ro\mouse.h
    Signed-off-by: X-EcutiOnner <xex.ecutionner@gmail.com>

--- Injection/Core/ro/mouse.h
+++ Injection/Core/ro/mouse.h
@@ -1,7 +1,6 @@
-
 enum EBtnState
 {
-	BTN_NONE,
+	BTN_NONE = 0,
 	BTN_DOWN,
 	BTN_PRESSED,
 	BTN_UP,
@@ -11,18 +10,17 @@ enum EBtnState
 class CMouse
 {
 public:
-	IDirectInput7*			m_lpdi;
-	IDirectInputDevice7*	m_pMouse;
-	LPVOID					m_hevtMouse;
-	int						m_xDelta;
-	int						m_yDelta;
-	int						m_xPos;
-	int						m_yPos;
-	int						m_wheel;
-	int						m_oldBtnState[3];
-	EBtnState				m_btnState[3];
-	int						m_dblclkCnt[3];
-	DWORD					m_dblclkTime;
-	DWORD					m_bSwapButton;
+	IDirectInput7*       m_lpdi;
+	IDirectInputDevice7* m_pMouse;
+	LPVOID               m_hevtMouse;
+	int                  m_xDelta;
+	int                  m_yDelta;
+	int                  m_xPos;
+	int                  m_yPos;
+	int                  m_wheel;
+	int                  m_oldBtnState[3];
+	EBtnState            m_btnState[3];
+	int                  m_dblclkCnt[3];
+	DWORD                m_dblclkTime;
+	DWORD                m_bSwapButton;
 };
-
