3ce2f59e214f3a9e5cd5c1ab93c2ec4b164b2308
X-EcutiOnner <xex.ecutionner@gmail.com>
2020-12-06 19:51:35 +0700

    Update coding style in Injection\Core\FastFont\CacheInfo.h
    Signed-off-by: X-EcutiOnner <xex.ecutionner@gmail.com>

--- Injection/Core/FastFont/CacheInfo.h
+++ Injection/Core/FastFont/CacheInfo.h
@@ -1,45 +1,47 @@
 #pragma once
 
-typedef long            s32;
-typedef unsigned long   u32;
-typedef short           s16;
-typedef unsigned short  u16;
-typedef char            s8;
-typedef unsigned char   u8;
+typedef long           s32;
+typedef unsigned long  u32;
+typedef short          s16;
+typedef unsigned short u16;
+typedef char           s8;
+typedef unsigned char  u8;
 
 #ifndef NULL
-#define NULL 0
+	#define NULL 0
 #endif
 
 class CacheInfo
 {
-	typedef struct StCacheInfo{
-		u32   OriginalKey;
-		u32   Value;
-		void  *pData;
-		//
+	typedef struct StCacheInfo
+	{
+		u32 OriginalKey;
+		u32 Value;
+
+		void *pData;
+
 		StCacheInfo *pPrev;
 		StCacheInfo *pNext;
-		//
+
 		StCacheInfo *pPrevHash;
 		StCacheInfo *pNextHash;
-	}StCacheInfo;
+	} StCacheInfo;
+
 public:
-	//CacheInfo();
+	// CacheInfo();
 	CacheInfo(int HashTopTables);
 	virtual ~CacheInfo(void);
 
 	void ClearCache(void);
-	void *CreateData(int hashkey,int datasize);
+	void *CreateData(int hashkey, int datasize);
 	void *GetCacheData(int hashkey);
 
-	int  DebugGetHashEntrys(int hashtableno);
+	int DebugGetHashEntrys(int hashtableno);
 
 private:
-	int         m_CacheNums;
-	int         m_HashRootTables;
+	int m_CacheNums;
+	int m_HashRootTables;
 
 	StCacheInfo m_CacheRoot;
 	StCacheInfo *m_HashRootTable;
-
 };
