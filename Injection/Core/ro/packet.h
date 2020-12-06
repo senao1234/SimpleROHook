d07b854b052751ff4316ee4998e1ffa04c45f8b1
X-EcutiOnner <xex.ecutionner@gmail.com>
2020-12-06 22:20:37 +0700

    Update coding style in Injection\Core\ro\packet.h
    Signed-off-by: X-EcutiOnner <xex.ecutionner@gmail.com>

--- Injection/Core/ro/packet.h
+++ Injection/Core/ro/packet.h
@@ -1,22 +1,24 @@
-enum PACKET_HEADER{
+enum PACKET_HEADER
+{
 	HEADER_ZC_SAY_DIALOG = 0xb4,
-	HEADER_ZC_MENU_LIST = 0xb7,
+	HEADER_ZC_MENU_LIST  = 0xb7,
 };
 
-
-#pragma pack(push,1)
+#pragma pack(push, 1)
 
 #pragma warning(push)
 #pragma warning(disable : 4200)
 
-struct PACKET_CZ_SAY_DIALOG {
+struct PACKET_CZ_SAY_DIALOG
+{
 	WORD  PacketType;
 	WORD  PacketLength;
 	DWORD AID;
 	BYTE  Data[];
 };
 
-struct PACKET_CZ_MENU_LIST {
+struct PACKET_CZ_MENU_LIST
+{
 	WORD  PacketType;
 	WORD  PacketLength;
 	DWORD AID;
