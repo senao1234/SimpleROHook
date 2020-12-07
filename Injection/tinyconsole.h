82cd36082e740b5c52c96713cf93aa9bd0789556
X-EcutiOnner <xex.ecutionner@gmail.com>
2020-12-07 19:15:52 +0700

    Update coding style in Injection\tinyconsole.h
    Signed-off-by: X-EcutiOnner <xex.ecutionner@gmail.com>

--- Injection/tinyconsole.h
+++ Injection/tinyconsole.h
@@ -4,19 +4,20 @@ void DebugLogger(const char* format, ...);
 void DebugLoggerWithLogWindow(const char* format, ...);
 
 #define DEBUG_LOGGING_NORMAL(a) DebugLoggerWithLogWindow a
-//#define DEBUG_LOGGING_DETAIL(a) DebugLoggerWithLogWindow a
-//#define DEBUG_LOGGING_MORE_DETAIL(a) DebugLogger a
+// #define DEBUG_LOGGING_DETAIL(a) DebugLoggerWithLogWindow a
+// #define DEBUG_LOGGING_MORE_DETAIL(a) DebugLogger a
 
 #ifndef DEBUG_LOGGING_NORMAL
 	#define DEBUG_LOGGING_NORMAL(a)
 #endif
+
 #ifndef DEBUG_LOGGING_DETAIL
 	#define DEBUG_LOGGING_DETAIL(a)
 #endif
+
 #ifndef DEBUG_LOGGING_MORE_DETAIL
 	#define DEBUG_LOGGING_MORE_DETAIL(a)
 #endif
 
 void CreateTinyConsole(void);
 void ReleaseTinyConsole(void);
-
