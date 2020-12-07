f2f4100860aa5570ea7dd7192aa625cd93c4f63a
X-EcutiOnner <xex.ecutionner@gmail.com>
2020-12-07 13:55:22 +0700

    Update coding style in SimpleROHookCS\SRHAboutBox.cs
    Signed-off-by: X-EcutiOnner <xex.ecutionner@gmail.com>

--- SimpleROHookCS/SRHAboutBox.cs
+++ SimpleROHookCS/SRHAboutBox.cs
@@ -22,20 +22,20 @@ namespace SimpleROHookCS
         }
 
         #region Assembly attribute accessor
-
         public string AssemblyTitle
         {
             get
             {
                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+
                 if (attributes.Length > 0)
                 {
                     AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+
                     if (titleAttribute.Title != "")
-                    {
                         return titleAttribute.Title;
-                    }
                 }
+
                 return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
             }
         }
@@ -53,10 +53,10 @@ namespace SimpleROHookCS
             get
             {
                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+
                 if (attributes.Length == 0)
-                {
                     return "";
-                }
+
                 return ((AssemblyDescriptionAttribute)attributes[0]).Description;
             }
         }
@@ -66,10 +66,10 @@ namespace SimpleROHookCS
             get
             {
                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+
                 if (attributes.Length == 0)
-                {
                     return "";
-                }
+
                 return ((AssemblyProductAttribute)attributes[0]).Product;
             }
         }
@@ -79,10 +79,10 @@ namespace SimpleROHookCS
             get
             {
                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+
                 if (attributes.Length == 0)
-                {
                     return "";
-                }
+
                 return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
             }
         }
@@ -92,13 +92,14 @@ namespace SimpleROHookCS
             get
             {
                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+
                 if (attributes.Length == 0)
-                {
                     return "";
-                }
+
                 return ((AssemblyCompanyAttribute)attributes[0]).Company;
             }
         }
         #endregion
+
     }
 }
