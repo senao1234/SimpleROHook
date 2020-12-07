e995ddf0788a259686d542a23aa5969a40ec3849
X-EcutiOnner <xex.ecutionner@gmail.com>
2020-12-07 21:26:53 +0700

    Update coding style in Injection\ProxyIDirectInput.cpp
    Signed-off-by: X-EcutiOnner <xex.ecutionner@gmail.com>

--- Injection/ProxyIDirectInput.cpp
+++ Injection/ProxyIDirectInput.cpp
@@ -3,17 +3,18 @@
 
 #include "Core/RoCodeBind.h"
 
-HRESULT CProxyIDirectInput7::Proxy_CreateDevice(THIS_ REFGUID rguid,LPDIRECTINPUTDEVICEA *lpIDD,LPUNKNOWN pUnkOuter)
+HRESULT CProxyIDirectInput7::Proxy_CreateDevice(THIS_ REFGUID rguid, LPDIRECTINPUTDEVICEA *lpIDD, LPUNKNOWN pUnkOuter)
 {
 	DEBUG_LOGGING_MORE_DETAIL(("IDirectInput7::CreateDevice()\n"));
 
 	void *ret_cProxy;
-	IDirectInputDevice7* lpDirectInputDevice7;
+	IDirectInputDevice7 *lpDirectInputDevice7;
 
-	HRESULT Result = m_Instance->CreateDevice(rguid, (LPDIRECTINPUTDEVICEA*)&lpDirectInputDevice7,pUnkOuter);
+	HRESULT Result = m_Instance->CreateDevice(rguid, (LPDIRECTINPUTDEVICEA*) & lpDirectInputDevice7, pUnkOuter);
 
-	if ( IsEqualGUID(rguid, GUID_SysMouse) ){
-		DEBUG_LOGGING_MORE_DETAIL(("IDirectInput7::Hook_CProxyIDirectInputDevice7 0x%0x ", lpDirectInputDevice7));
+	if (IsEqualGUID(rguid, GUID_SysMouse))
+	{
+		DEBUG_LOGGING_MORE_DETAIL(("IDirectInput7::Hook_CProxyIDirectInputDevice7 = 0x%0x", lpDirectInputDevice7));
 		ret_cProxy = (void*)(new CProxyIDirectInputDevice7(lpDirectInputDevice7));
 		*lpIDD = (LPDIRECTINPUTDEVICEA)ret_cProxy;
 	}
@@ -21,14 +22,14 @@ HRESULT CProxyIDirectInput7::Proxy_CreateDevice(THIS_ REFGUID rguid,LPDIRECTINPU
 	return Result;
 }
 
-HRESULT CProxyIDirectInputDevice7::Proxy_GetDeviceState(THIS_ DWORD cbData,LPVOID lpvData)
+HRESULT CProxyIDirectInputDevice7::Proxy_GetDeviceState(THIS_ DWORD cbData, LPVOID lpvData)
 {
 	static DWORD oldtime = 0;
 	HRESULT Result;
 
 	Result = m_Instance->GetDeviceState(cbData, lpvData);
 
-	if( g_pRoCodeBind )
+	if (g_pRoCodeBind)
 		g_pRoCodeBind->OneSyncProc(Result, lpvData, g_FreeMouseSw);
 
 	return Result;
@@ -39,10 +40,10 @@ HRESULT CProxyIDirectInputDevice7::Proxy_SetCooperativeLevel(HWND hwnd, DWORD dw
 	HRESULT Result;
 	DEBUG_LOGGING_MORE_DETAIL(("lpDI->SetCooperativeLevel\n"));
 
-	if( g_FreeMouseSw )
+	if (g_FreeMouseSw)
 		dwflags = DISCL_NONEXCLUSIVE | DISCL_BACKGROUND;
 
-	Result = m_Instance->SetCooperativeLevel(hwnd,dwflags);
+	Result = m_Instance->SetCooperativeLevel(hwnd, dwflags);
 
 	return Result;
 }
