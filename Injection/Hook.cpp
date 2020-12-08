004669d9f5589223a9c9263b3ab821830c489fc0
X-EcutiOnner <xex.ecutionner@gmail.com>
2020-12-08 17:30:17 +0700

    Trim trailing spaces in Injection\Hook.cpp
    Signed-off-by: X-EcutiOnner <xex.ecutionner@gmail.com>

--- Injection/Hook.cpp
+++ Injection/Hook.cpp
@@ -6,7 +6,7 @@
 HINSTANCE g_hDLL;
 
 // This segment must be defined as SHARED in the .DEF
-#pragma data_seg (".HookShared")		
+#pragma data_seg (".HookShared")
 // Shared instance for all processes.
 HHOOK g_hHook = NULL;
 #pragma data_seg()
@@ -14,14 +14,14 @@ HHOOK g_hHook = NULL;
 #pragma comment(linker, "/Section:.HookShared,rws")
 
 
-LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam) 
+LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    return ::CallNextHookEx( g_hHook, nCode, wParam, lParam); 
+    return ::CallNextHookEx( g_hHook, nCode, wParam, lParam);
 }
 
 INJECTION_API void InstallHook()
 {
-	g_hHook = ::SetWindowsHookEx( WH_CBT, HookProc, g_hDLL, 0 ); 
+	g_hHook = ::SetWindowsHookEx( WH_CBT, HookProc, g_hDLL, 0 );
 }
 
 INJECTION_API void RemoveHook()
@@ -29,5 +29,3 @@ INJECTION_API void RemoveHook()
     ::UnhookWindowsHookEx( g_hHook );
 	g_hHook = NULL;
 }
-
-
