97cc2345843ffa55fbdcc24c4589d4ad80ee653a
X-EcutiOnner <xex.ecutionner@gmail.com>
2022-11-03 22:39:00 +0700

    Update Coding Style in CSearchCode UIYourItemWnd__SendMsg_REQ_WEAR_EQUIP_Handler_TypeG

--- Injection/Core/RoCodeBind.cpp
+++ Injection/Core/RoCodeBind.cpp
@@ -1814,27 +1814,27 @@ void CRoCodeBind::SearchRagexeMemory(void)
 
 	// 2017-09-20ragexe Ragexe.exe iRO RE:Start
 	CSearchCode UIYourItemWnd__SendMsg_REQ_WEAR_EQUIP_Handler_TypeG(
-		"B9*1******"        // mov     ecx, ragexe.BB7468 ; CModeMgr g_modeMgr
-		"E8*2******"        // call    ragexe.59A910 ; CModeMgr::GetGameMode
-		"B898090000"        // mov     eax, 998
-		"668945**"          // mov     word ptr ss:[ebp-30], ax
-		"668B86********"    // mov     ax, word ptr ds:[esi+88]
-		"668945**"          // mov     word ptr ss:[ebp-2E], ax
-		"8B86********"      // mov     eax, dword ptr ds:[esi+8C]
-		"8945**"            // mov     dword ptr ss:[ebp-2C], eax
-		"8D45**"            // lea     eax, dword ptr ss:[ebp-30]
-		"50"                // push    eax
-		"6898090000"        // push    998
-		"E8*3******"        // call    ragexe.7CCAB0 ; CRagConnection::instanceR
-		"8BC8"              // mov     ecx, eax
-		"E8*4******"        // call    ragexe.7CC080 ; CRagConnection::GetPacketSize
-		"50"                // push    eax
-		"E8********"        // call    ragexe.7CCAB0 ; CRagConnection::instanceR
-		"8BC8"              // mov     ecx, eax
-		"E8*5******"        // call    ragexe.7CC810 ; CRagConnection::SendPacket
-		"688A000000"        // push    8A
-		"B9*6******"        // mov     ecx, ragexe.BE3578 ; UIWindowMgr g_windowMgr
-		"E8*7******"        // call    ragexe.5654E0 ; UIWindowMgr::DeleteWindow
+		"B9*1******"              ///< mov     ecx, ragexe.BB7468 ; CModeMgr g_modeMgr
+		"E8*2******"              ///< call    ragexe.59A910 ; CModeMgr::GetGameMode
+		"B898090000"              ///< mov     eax, 998
+		"668945**"                ///< mov     word ptr ss:[ebp-30], ax
+		"668B86********"          ///< mov     ax, word ptr ds:[esi+88]
+		"668945**"                ///< mov     word ptr ss:[ebp-2E], ax
+		"8B86********"            ///< mov     eax, dword ptr ds:[esi+8C]
+		"8945**"                  ///< mov     dword ptr ss:[ebp-2C], eax
+		"8D45**"                  ///< lea     eax, dword ptr ss:[ebp-30]
+		"50"                      ///< push    eax
+		"6898090000"              ///< push    998
+		"E8*3******"              ///< call    ragexe.7CCAB0 ; CRagConnection::instanceR
+		"8BC8"                    ///< mov     ecx, eax
+		"E8*4******"              ///< call    ragexe.7CC080 ; CRagConnection::GetPacketSize
+		"50"                      ///< push    eax
+		"E8********"              ///< call    ragexe.7CCAB0 ; CRagConnection::instanceR
+		"8BC8"                    ///< mov     ecx, eax
+		"E8*5******"              ///< call    ragexe.7CC810 ; CRagConnection::SendPacket
+		"688A000000"              ///< push    8A
+		"B9*6******"              ///< mov     ecx, ragexe.BE3578 ; UIWindowMgr g_windowMgr
+		"E8*7******"              ///< call    ragexe.5654E0 ; UIWindowMgr::DeleteWindow
 		);
 
 	CSearchCode Mov_ecx_adr_Call_near_adr(
