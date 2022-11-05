cba6887982b4ed174a24e974633744f7d4025c65
X-EcutiOnner <xex.ecutionner@gmail.com>
2022-11-05 14:05:27 +0700

    Update Settings.Designer.cs

--- SimpleROHookCS/Properties/Settings.Designer.cs
+++ SimpleROHookCS/Properties/Settings.Designer.cs
@@ -9,20 +9,19 @@
 //------------------------------------------------------------------------------
 
 namespace SimpleROHookCS.Properties {
-    
-    
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
+
         public static Settings Default {
             get {
                 return defaultInstance;
             }
         }
-        
+
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("0, 0, 200, 400")]
@@ -34,7 +33,7 @@ namespace SimpleROHookCS.Properties {
                 this["LoggerWinBounds"] = value;
             }
         }
-        
+
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("Normal")]
@@ -46,7 +45,7 @@ namespace SimpleROHookCS.Properties {
                 this["LoggerWinState"] = value;
             }
         }
-        
+
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
